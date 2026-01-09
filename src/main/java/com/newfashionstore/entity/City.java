8cd656d72ae177991037fd42c5d207b485f3b5d3
shazee-04 <mgssrangajeewa@gmail.com>
2026-01-09 22:02:55 +0530

    ✅ Add City entity class for city management

---
+++ src/main/java/com/newfashionstore/entity/City.java
@@ -0,0 +1,41 @@
+package com.newfashionstore.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "city")
+public class City implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public City() {
+    }
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
