bda8a08ebf8bfe944f638a50cb6c0322a77715ec
shazee-04 <mgssrangajeewa@gmail.com>
2026-01-10 19:06:52 +0530

    ✅ Add Status entity class for status management

---
+++ src/main/java/com/newfashionstore/entity/Status.java
@@ -0,0 +1,40 @@
+package com.newfashionstore.entity;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "status")
+public class Status implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    public Status() {
+    }
+
+    public Status(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
