763cbebdf1ff4cd9350f82d6c0103c91be3d3859
shazee-04 <mgssrangajeewa@gmail.com>
2026-01-10 19:06:02 +0530

    ✅ Add OrderStatus entity class for order status management

---
+++ src/main/java/com/newfashionstore/entity/OrderStatus.java
@@ -0,0 +1,37 @@
+package com.newfashionstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "order_status")
+public class OrderStatus implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    public OrderStatus() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
