d8cecf04a6a1309d06d53deda925b399040c473c
shazee-04 <mgssrangajeewa@gmail.com>
2026-01-10 19:04:07 +0530

    ✅ Add Brand entity class for brand management

---
+++ src/main/java/com/newfashionstore/entity/Brand.java
@@ -0,0 +1,37 @@
+package com.newfashionstore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "brand")
+public class Brand implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    public Brand() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
