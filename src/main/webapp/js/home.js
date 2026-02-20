9139fd13c4bab7dde4c58f7e46e2adae24ac2d25
shazee-04 <mgssrangajeewa@gmail.com>
2026-02-20 12:57:21 +0530

    ✅ Refactor swiper slide structure and update styles for banner content

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -24,19 +24,19 @@ function renderHeroSwiper(banners) {
         const bannerImage = b.imagePath || 'assets/images/banner-placeholder.jpg';
 
         swiperWrapper.innerHTML += `
-        <div class="swiper-slide">
-            <div class="row banner-item text-center align-items-center">
-                <div class="col-lg-6">
-                    <div class="image-holder">
-                        <img src="${bannerImage}" alt="product" class="product-image img-fluid"
-                        onerror="this.onerror=null;this.src='assets/images/banner-placeholder.jpg';">
+        <div class="swiper-slide d-flex align-items-center"
+             style="background-image:url(${bannerImage});">
+            <div class="banner-content w-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 offset-md-6">
+                            <h2 class="display-1 text-uppercase mt-5 pt-5">${b.title}</h2>
+                            <p class="caption">${b.description}</p>
+                            <div class="btn-left btn-swiper">
+                                <a href="${b.url}" class="btn btn-light text-uppercase mt-3">Shop Collection</a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="banner-content col-lg-6 p-5">
-                    <h2 class="display-2 text-uppercase txt-fx slide-up">${b.title}</h2>
-                    <p>${b.description}</p>
-                    <a href="${b.url}" class="btn btn-outline-dark text-uppercase mt-3">Shop
-                        Collection</a>
                 </div>
             </div>
         </div>
