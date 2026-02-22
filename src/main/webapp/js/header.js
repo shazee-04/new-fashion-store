909abd5f28a0eb575e1675b81e0cdc5a1da867b6
shazee-04 <mgssrangajeewa@gmail.com>
2026-02-22 21:26:29 +0530

    ✅ Update cart offcanvas to display item count and skeleton loading states

--- src/main/webapp/js/header.js
+++ src/main/webapp/js/header.js
@@ -194,7 +194,7 @@ class Header extends HTMLElement {
     <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasCart">
         <div class="offcanvas-header justify-content-between align-items-center">
             <h5 class="offcanvas-title" id="offcanvasCartLabel">
-                Your Cart <span class="fs-6 text-muted">(3)</span>
+                Your Cart <span class="fs-6 text-muted" id="offcanvasCart-item-count">(0)</span>
             </h5>
             <button type="button" class="text-reset shadow-none border-0 bg-transparent" data-bs-dismiss="offcanvas"
                 aria-label="Close">
@@ -204,93 +204,41 @@ class Header extends HTMLElement {
 
         <div class="offcanvas-body">
             <div class="order-md-last">
-                <ul class="list-group mb-3">
+                <ul class="list-group mb-3" id="offcanvasCart-items-container">
                     <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <div class="d-flex align-items-start gap-2">
-                            <img src="assets/images/product-item1.jpg" class="object-fit-cover"
-                                style="aspect-ratio: 1 / 1; height: 100px;" alt="Product image">
-                            <div class="p-1">
-                                <h6 class="my-0 cart-item-title">Growers cider</h6>
-                                <p class="text-muted cart-item-desc small mb-1">
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe dolor quia beatae
-                                    dolores ab ea unde eligendi debitis quisquam cupiditate, at culpa consequuntur
-                                    incidunt earum vitae in quaerat aspernatur aliquid odio doloremque corrupti, sint
-                                    autem facere ad. Dolore totam nobis dolorum necessitatibus.
-                                </p>
-                                <span class="cart-item-qty">1 x $12</span>
-                            </div>
+                        <div class="skeleton" style="height: 100px; aspect-ratio: 1/1;"></div>
+                        <div class="w-100 ms-3">
+                            <div class="skeleton mb-3 p-1 pb-3"></div>
+                            <div class="skeleton mb-1 p-2 w-75"></div>
+                            <div class="skeleton mb-3 p-2 w-50"></div>
                         </div>
-                        <span class="text-muted cart-item-price">
-                            $120000
-                        </span>
                     </li>
                     <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <div class="d-flex align-items-start gap-2">
-                            <img src="assets/images/product-item1.jpg" class="object-fit-cover"
-                                style="aspect-ratio: 1 / 1; height: 100px;" alt="Product image">
-                            <div class="p-1">
-                                <h6 class="my-0 cart-item-title">Growers cider</h6>
-                                <p class="text-muted cart-item-desc small mb-1">
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe dolor quia beatae
-                                    dolores ab ea unde eligendi debitis quisquam cupiditate, at culpa consequuntur
-                                    incidunt earum vitae in quaerat aspernatur aliquid odio doloremque corrupti, sint
-                                    autem facere ad. Dolore totam nobis dolorum necessitatibus.
-                                </p>
-                                <span class="cart-item-qty">1 x $12</span>
-                            </div>
+                        <div class="skeleton" style="height: 100px; aspect-ratio: 1/1;"></div>
+                        <div class="w-100 ms-3">
+                            <div class="skeleton mb-3 p-1 pb-3"></div>
+                            <div class="skeleton mb-1 p-2 w-75"></div>
+                            <div class="skeleton mb-3 p-2 w-50"></div>
                         </div>
-                        <span class="text-muted cart-item-price">
-                            $120000
-                        </span>
                     </li>
                     <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <div class="d-flex align-items-start gap-2">
-                            <img src="assets/images/product-item1.jpg" class="object-fit-cover"
-                                style="aspect-ratio: 1 / 1; height: 100px;" alt="Product image">
-                            <div class="p-1">
-                                <h6 class="my-0 cart-item-title">Growers cider</h6>
-                                <p class="text-muted cart-item-desc small mb-1">
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe dolor quia beatae
-                                    dolores ab ea unde eligendi debitis quisquam cupiditate, at culpa consequuntur
-                                    incidunt earum vitae in quaerat aspernatur aliquid odio doloremque corrupti, sint
-                                    autem facere ad. Dolore totam nobis dolorum necessitatibus.
-                                </p>
-                                <span class="cart-item-qty">1 x $12</span>
-                            </div>
+                        <div class="skeleton" style="height: 100px; aspect-ratio: 1/1;"></div>
+                        <div class="w-100 ms-3">
+                            <div class="skeleton mb-3 p-1 pb-3"></div>
+                            <div class="skeleton mb-1 p-2 w-75"></div>
+                            <div class="skeleton mb-3 p-2 w-50"></div>
                         </div>
-                        <span class="text-muted cart-item-price">
-                            $120000
-                        </span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-sm">
-                        <div class="d-flex align-items-start gap-2">
-                            <img src="assets/images/product-item1.jpg" class="object-fit-cover"
-                                style="aspect-ratio: 1 / 1; height: 100px;" alt="Product image">
-                            <div class="p-1">
-                                <h6 class="my-0 cart-item-title">Growers cider</h6>
-                                <p class="text-muted cart-item-desc small mb-1">
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe dolor quia beatae
-                                    dolores ab ea unde eligendi debitis quisquam cupiditate, at culpa consequuntur
-                                    incidunt earum vitae in quaerat aspernatur aliquid odio doloremque corrupti, sint
-                                    autem facere ad. Dolore totam nobis dolorum necessitatibus.
-                                </p>
-                                <span class="cart-item-qty">1 x $12</span>
-                            </div>
-                        </div>
-                        <span class="text-muted cart-item-price">
-                            $120000
-                        </span>
                     </li>
                 </ul>
 
                 <div class="list-group-item d-flex justify-content-between text-dark py-2 fw-medium">
                     <span>Total (LKR)</span>
-                    <span>20000</span>
+                    <span id="offcanvasCart-total">-</span>
                 </div>
-                <button class="w-100 btn btn-outline-dark btn-lg" type="submit">
-                    Continue to checkout
+                <a href="cart.html" class="w-100 btn btn-outline-dark btn-lg">
+                    View full cart
                     <i class="bi bi-arrow-right"></i>
-                </button>
+                </a>
             </div>
         </div>
     </div>
