66d4a5b06cbfce08006443c1276977e9dc805eb9
shazee-04 <mgssrangajeewa@gmail.com>
2026-03-12 20:17:57 +0530

    ✅ Add PaymentMethod class to represent payment method data with ID and name attributes

---
+++ src/main/java/com/newfashionstore/entity/PaymentMethod.java
@@ -0,0 +1,32 @@
+package com.newfashionstore.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "payment_method")
+public class PaymentMethod {
+    @Id
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
