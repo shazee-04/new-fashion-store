b54cf790c1101bddcfa0f12b05aa85a3e49ac48f
shazee-04 <mgssrangajeewa@gmail.com>
2026-06-05 21:08:01 +0530

    ✅ Update AppConfig: include admin controllers package for resource scanning

--- src/main/java/com/newfashionstore/config/AppConfig.java
+++ src/main/java/com/newfashionstore/config/AppConfig.java
@@ -5,6 +5,7 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class AppConfig extends ResourceConfig {
     public AppConfig() {
         packages("com.newfashionstore.controller");
+        packages("com.newfashionstore.controller.admin");
         packages("com.newfashionstore.filter");
         packages("com.newfashionstore.util");
         register(org.glassfish.jersey.media.multipart.MultiPartFeature.class);
